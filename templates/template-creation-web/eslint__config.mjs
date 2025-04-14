ef52eed0f17b84ed1a0eec4b5a984807766d4a9c
PaulKokhanov1 <69466838+PaulKokhanov1@users.noreply.github.com>
2025-04-14 17:18:22 +0900

    add prettier to template bundler vite

--- templates/template-creation-web/eslint__config.mjs
+++ templates/template-creation-web/eslint__config.mjs
@@ -1,14 +1,19 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier,
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
     rules: {},
   },
